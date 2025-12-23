b194d6bf8311caad343907ac47c528ef90188239
EvilSupahFly <seann.giffin@gmail.com>
2025-12-23 18:44:07 -0500

    Minor presentation bug fix
    GRAB_EXE: full scan list
    SELECTED_EXES: user-approved subset (order preserved)

--- WINE-Game-Install-Script-for-Linux/install.sh
+++ WINE-Game-Install-Script-for-Linux/install.sh
@@ -753,6 +753,9 @@ EOL
 fi
 
 # Make the script executable and present a recap
+# GRAB_EXE: full scan list
+# SELECTED_EXES: user-approved subset (order preserved)
+
 chmod a+x "$GSS"
 echo -e "\n"
 cat "$GSS"
@@ -765,8 +768,8 @@ echo -e "It probably won't cause any problems for non-nVidia GPUs, but it's best
 echo -e "The full path of your ${YELLOW}$ONE ${WHITE}wineprefix is: \"${YELLOW}$WINEPREFIX${WHITE}\""
 echo -e "Be sure to verify that the game executable(s) written to \"${YELLOW}$GSS${WHITE}\" as:${YELLOW}\n"
 for i in "${!SELECTED_EXES[@]}"; do
-    #echo -e "    $((i)): ${SELECTED_EXES[$i]}"
-    echo -e "    $((i)): ${GRAB_EXE[$i]}"
+    echo -e "    $((i+1)): ${SELECTED_EXES[$i]}"
+    #echo -e "    $((i+1)): ${GRAB_EXE[$i]}"
 done
 echo -e "\n${WHITE}and modify if necessary.${RESET}\n"
 if [ "$DBG" = "true" ]; then
